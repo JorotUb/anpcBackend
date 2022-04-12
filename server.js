ae81c60a1d557a9457443a21bcb0d586d14e63b9
JorotUb <67773612+JorotUb@users.noreply.github.com>
2022-04-12 23:45:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.get('/server',(req, res) => {
     console.log(req.body)
 })
 
-app.listen(4000, ()=> {
-    console.log("running on port 4000")
-})
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
